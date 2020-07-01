b009eb6a832b66f2265e95d2502c67ef0af993b0
Casco-csv <gabubicho@gmail.com>
2020-07-01 05:24:51 -0300

    eva01

---
+++ build/precache-manifest.5fc964dbfbfcb6ab86ef7448c581a3cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926f25c103d1e958738ee38c32cf5e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c60c0f6c8ab339c664ee",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "98cd823ff36b262bae1b",
+    "url": "/static/css/main.3e2d0970.chunk.css"
+  },
+  {
+    "revision": "c60c0f6c8ab339c664ee",
+    "url": "/static/js/2.f4dde6f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4dde6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cd823ff36b262bae1b",
+    "url": "/static/js/main.8bf2d1fd.chunk.js"
+  },
+  {
+    "revision": "0eaacdf52b71708d5f4c",
+    "url": "/static/js/runtime-main.01dd8bff.js"
+  },
+  {
+    "revision": "d21203ad3785860296137eed5eed1922",
+    "url": "/static/media/head-img.d21203ad.png"
+  }
+]);
